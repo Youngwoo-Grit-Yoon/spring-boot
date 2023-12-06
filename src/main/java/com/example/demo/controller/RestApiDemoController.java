ce87d8bc1ec90c79b25e7b50de65702d9d4fb095
윤영우 <youngwooyoon@yoon-young-woo-MacBookPro.local>
2023-12-07 01:10:16 +0900

    Refactor put method

--- src/main/java/com/example/demo/controller/RestApiDemoController.java
+++ src/main/java/com/example/demo/controller/RestApiDemoController.java
@@ -1,12 +1,20 @@
 package com.example.demo.controller;
 
 import com.example.demo.domain.Solution;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * IETF 문서의 HTTP 상태 코드
+ * 1. GET 메서드에는 특정 상태 코드를 지정하지 않는다.
+ * 2. POST와 DELETE 메서드에는 상태 코드 사용을 권장한다.
+ * 3. PUT 메서드에는 응답 시 상태 코드 사용이 필수다.
+ */
 @RestController
 @RequestMapping("/cc/solutions")
 public class RestApiDemoController {
@@ -47,7 +55,7 @@ public class RestApiDemoController {
     }
 
     @PutMapping("/{id}")
-    Solution putSolution(@PathVariable String id, @RequestBody Solution solution) {
+    ResponseEntity<Solution> putSolution(@PathVariable String id, @RequestBody Solution solution) {
         int solutionIndex = -1;
 
         for (Solution s : solutions) {
@@ -57,7 +65,9 @@ public class RestApiDemoController {
             }
         }
 
-        return (solutionIndex == -1) ? postSolution(solution) : solution;
+        return (solutionIndex == -1) ?
+                new ResponseEntity<>(postSolution(solution), HttpStatus.CREATED) :
+                new ResponseEntity<>(solution, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
