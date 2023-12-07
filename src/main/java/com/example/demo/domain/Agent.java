3365311bdab9a845b09b8b820ab9d9a7827fd4bd
윤영우 <youngwooyoon@yoon-young-woo-MacBookPro.local>
2023-12-07 23:21:32 +0900

    Add Agent class

---
+++ src/main/java/com/example/demo/domain/Agent.java
@@ -0,0 +1,21 @@
+package com.example.demo.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Agent {
+    @Id
+    private String id;
+    private String name;
+
+    /**
+     * JPA를 사용해 DB에 데이터를 생성할 때는 기본 생성자가 필요하다.
+     */
+    public Agent() {
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
