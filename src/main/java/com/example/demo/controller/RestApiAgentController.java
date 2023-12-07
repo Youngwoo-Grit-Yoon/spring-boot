468f7cc01c30a523fc38678288c375ae18a1b0f7
윤영우 <youngwooyoon@yoon-young-woo-MacBookPro.local>
2023-12-08 00:35:49 +0900

    Add methods

--- src/main/java/com/example/demo/controller/RestApiAgentController.java
+++ src/main/java/com/example/demo/controller/RestApiAgentController.java
@@ -2,11 +2,12 @@ package com.example.demo.controller;
 
 import com.example.demo.domain.Agent;
 import com.example.demo.repository.AgentRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/agent")
@@ -26,5 +27,28 @@ public class RestApiAgentController {
     @GetMapping
     Iterable<Agent> getAgents() {
         return this.agentRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    Optional<Agent> getAgentById(@PathVariable String id) {
+        return this.agentRepository.findById(id);
+    }
+
+    @PostMapping
+    Agent postAgent(@RequestBody Agent agent) {
+        return this.agentRepository.save(agent);
+    }
+
+    @PutMapping("/{id}")
+    ResponseEntity<Agent> putAgent(@PathVariable String id,
+                                   @RequestBody Agent agent) {
+        return (!this.agentRepository.existsById(id)) ?
+                new ResponseEntity<>(this.agentRepository.save(agent), HttpStatus.CREATED) :
+                new ResponseEntity<>(this.agentRepository.save(agent), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    void deleteAgent(@PathVariable String id) {
+        this.agentRepository.deleteById(id);
     }
 }
