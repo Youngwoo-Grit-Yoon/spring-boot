ec8eab38ac9ce27738bdc6793764ff8eddb7b536
윤영우 <youngwooyoon@yoon-young-woo-MacBookPro.local>
2023-12-08 14:56:07 +0900

    Add CallInfraSystem configuration

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,11 @@
 package com.example.demo;
 
+import com.example.demo.config.CallInfraSystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -12,4 +15,9 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	@ConfigurationProperties(prefix = "call-infra-system")
+	CallInfraSystem createCallInfraSystem() {
+		return new CallInfraSystem();
+	}
 }
---
+++ src/main/java/com/example/demo/config/CallInfraSystem.java
@@ -0,0 +1,22 @@
+package com.example.demo.config;
+
+public class CallInfraSystem {
+    private String address;
+    private int port;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+}
